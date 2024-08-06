a0bf90d9921b8a710bb67b168cf55c1c754ec44c
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-06 13:47:50 +0300

    methods is done

---
+++ Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        // genellikle kendini tekrarlama( donnt repaet yourself ) durumlarında kullanılır
+
+        static void Main(string[] args)
+        {
+           var result = Add(32, 45); // methodu çağırıyoruz
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static int Add(int v1 , int v2) // şeklinde methodu tanımlıyoruz.
+        {
+            return v1 + v2;  // geri dönüş değer tipi static'ten sonra belirtiyoruz.
+        }
+        
+        static int Add2(int v1, int v2=25) // şeklinde methodu tanımlıyoruz.
+        {
+            // yukarıda değer girilmez ise v2 için 25 ataması yapılacaktır. tek girdi ile çalışabilir.
+
+            return v1 + v2;  // geri dönüş değer tipi static'ten sonra belirtiyoruz.
+        }
+         // default değerli parametreli method
+
+        static int  Add3(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+
+
+    }
+}
