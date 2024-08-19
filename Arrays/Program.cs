377468ef54d8a975e49f5ddc5e1892e699ec7cee
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-19 17:41:02 +0300

    Arrays is done

---
+++ Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Öğrenci isimleri
+            string[] students = { "Engin", "Mehmet", "Ali" };
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            // Türkiye'nin bölgeleri ve büyük şehirleri (2 boyutlu dizi)
+            string[,] regions =
+            {
+                { "İstanbul", "İzmit", "Balıkesir" },
+                { "Ankara", "Konya", "Kırıkkale" },
+                { "Antalya", "Adana", "Mersin" },
+                { "Rize", "Trabzon", "Samsun" },
+                { "İzmir", "Muğla", "Manisa" }
+            };
+
+            // Tüm şehirleri listele
+            for (int i = 0; i < regions.GetLength(0); i++)
+            {
+                for (int j = 0; j < regions.GetLength(1); j++)
+                {
+                    Console.WriteLine(regions[i, j].Trim()); // Boşlukları kaldır
+                }
+            }
+        }
+    }
+}
