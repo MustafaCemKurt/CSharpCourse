1e4efbd66ecb10010252261be689b6297972995d
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-20 15:17:03 +0300

    Classes is done

---
+++ Classes/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Customer
+    {
+
+        // property 
+        public int Id { get; set; } // genelde database üzerinde bilgi tutmak içi n kullanılır.
+
+        private string name;
+        public string FirstName { get; set; }
+   
+
+        public string LastName  { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Region { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; } 
+
+        public string HomePhone { get; set; }
+
+        public string Extension { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        // property 
+
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // yapılmak istenene işlemleri gruplara ayırmak için kullanılır. 
+
+            CustommerManager manager = new CustommerManager(); // class'tan yeni bir nesne oluşturuyuoruz
+            manager.Add();
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "KENAN",
+                Country = "Turkey",
+                City = "Adana",
+                LastName="Ahmet",
+                PhoneNumber="0565478548554"
+
+            };
+
+
+
+
+
+
+
+
+        }
+
+        class CustommerManager
+        {
+
+            // iki fgarklı method oluştursalım
+            public void Add()
+            {
+                Console.WriteLine("added");
+
+            }
+            public void Update()
+            {
+                Console.WriteLine("customer updated");
+            }
+        }
+
+
+
+
+
+    }
+}
