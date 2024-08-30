f2b5324a64ad52e385c967580ce3944ce5dbb3a6
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-30 13:18:35 +0300

    ado.net tamamlandı

---
+++ AdoNetDemo/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class Product
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public string Amount { get; set; }
+
+        public static implicit operator Product(Product v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+
+        public DataTable GetAll2()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+            if(connection.State==ConnectionState.Closed)
+
+            {
+                connection.Open();
+
+            }
+            SqlCommand command = new SqlCommand("Select * from Products",connection);
+           SqlDataReader reader =  command.ExecuteReader();
+
+           DataTable datatable = new DataTable();   
+            datatable.Load(reader); 
+            reader.Close();
+            connection.Close();
+            return datatable;
+
+
+
+        }
+
+
+
+
+        public List<Product> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+            if (connection.State == ConnectionState.Closed)
+
+            {
+                connection.Open();
+
+            }
+            SqlCommand command = new SqlCommand("Select * from Products", connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                
+                Product product= new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    Amount = Convert.ToInt32(reader["Amount"]),
+                    Price = Convert.ToDecimal(reader["Price"])
+                     
+
+                    
+                    
+
+                    
+                };
+                products.Add(product);  
+
+
+            }
+
+
+            reader.Close();
+            connection.Close();
+           return products;
+
+
+
+        }
+
+
+
+
+
+    }
+}
