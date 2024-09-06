b35aea104851f7821fe452e823f26c1f92e87632
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-06 15:51:15 +0300

    reflections done

---
+++ Reflecitons/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflecitons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem=new DortIslem(3,6);
+            dortIslem.Topla2();
+            // dortIslem.Topla(); dediğimizde bizden sayi isteyecektir
+             
+
+            Console.ReadLine();
+        }
+    }
+
+    class DortIslem
+    {
+       private  int _sayi1;
+       private  int _sayi2;
+
+        public DortIslem(int sayi1, int sayi2)
+        {
+             _sayi1 = sayi1;
+            _sayi2 = sayi2; 
+        }
+        public int Topla(int sayi1 , int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+
+
+
+    }
+
+}
