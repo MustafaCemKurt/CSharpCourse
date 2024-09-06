339e94d9221570bdc6978a46ab0eccbea4c6c31c
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-06 15:16:42 +0300

    generic sınıflar tamamlandı

---
+++ Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IProduct 
+    {
+       
+    }
+
+    interface IRepository<T> where T: class // generic bir interface tanımlıyoruz
+    {
+        List<T> GetAll();
+        T Get (int id);
+
+        void Add (T item); // genel bir tanımlama yapıyoruz. girdiye göre işlemleri gerçekleştirir 
+
+
+        void Update (T item);   
+
+        void Delete (T item);   
+
+         
+
+    }
+
+    // interface IStudentDal:IRepository<string> // yazamamasını istiyoruz diyelim 
+    // interface IRepository<T> where T: class şeklinde yukarıda kısıtlama tanımlayabiliriz 
+    // sadece değer tipi istiyorsak interface IRepository<T> where T:struct şeklşinde tanımnlanır . 
+    // struct değer tiplerine karşılık gelir 
+
+
+
+    // generic kısıtları 
+    // generic olan bir sınıfı kısıtlama imkanı sağlar 
+
+}
